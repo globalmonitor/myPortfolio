28ca0dd97df03460e0d6b6335f29e895454fba56
pavangv28 <gvpavankumar2000@gmail.com>
2021-01-15 10:10:50 +0530

    First commit

---
+++ src/components/Banner.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import {
+    FaFacebookF,
+    FaTwitter,
+    FaInstagram,
+    FaLinkedin,
+  } from "react-icons/fa";
+const Banner = () => {
+    const [state]= React.useState({title: 'I am G V Pavan Kumar',text:'A Undergraduate student from Osmania University!!'
+    });
+    return (
+        
+    <header className="header">
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <div className="header__content">
+              <div className="header__section">
+                <ul className="header__ul">
+                  <li>
+                    <FaFacebookF className="headerIcon" />
+                  </li>
+                  <li>
+                    <FaTwitter className="headerIcon" />
+                  </li>
+                  
+                  <li>
+                    <FaInstagram className="headerIcon" />
+                  </li>
+                  <li>
+                    <FaLinkedin className="headerIcon" />
+                  </li>
+                </ul>
+                <h1>{state.title}</h1>
+                <p> {state.text}</p>
+                <div className="header__button">
+                    <a href="" className="btn btn-outline">My Portfolio
+                    </a>
+                    
+                    
+                </div>
+               </div>
+              </div>
+            </div>
+            </div>
+        </div>
+        
+    </header>
+
+    )
+}
+export default Banner
---
+++ src/components/Services.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Services = () => {
+    return (
+        <div className="services">
+            <div className="container">
+                <div className="services__header">
+                    <h3 className="heading">SERVICES</h3>
+                    <h1 className="mainHeader">
+                        My Services
+                    </h1>
+                    <p className="mainContent">
+                        
+                    </p>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Services
